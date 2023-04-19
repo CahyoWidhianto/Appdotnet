74a45ad05c4614426b4a1b9554184a955e28383b
CahyoWidhianto <widhiantoc@gmail.com>
2023-04-19 18:24:31 +0700

    Update Club && Race

--- Latihan/Controllers/RaceController.cs
+++ Latihan/Controllers/RaceController.cs
@@ -1,4 +1,5 @@
 ﻿using Latihan.Data;
+using Latihan.Data.Enum;
 using Latihan.Interfaces;
 using Latihan.Models;
 using Latihan.Repository;
@@ -62,5 +63,62 @@ namespace Latihan.Controllers
             }
             return View(raceVM);
         }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var race = await _raceRepository.GetByIdAsync(id);
+            if (race == null) return View("Error");
+            var raceVm = new EditRaceViewModel
+            {
+                Title = race.Title,
+                Description = race.Description,
+                AddressId = (int)race.AddressId,
+                Address = race.Address,
+                URL = race.Image,
+                RaceCategory = race.RaceCategory,
+            };
+            return View(raceVm);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, EditRaceViewModel raceVM)
+        {
+            if (!ModelState.IsValid)
+            {
+                ModelState.AddModelError("", "Failed to edit race");
+                return View("Edit", raceVM);
+            }
+            var userRace = await _raceRepository.GetByIdAsyncNoTracking(id);
+            if (userRace != null)
+            {
+                try
+                {
+                    await _photoService.DeletePhotoAsync(userRace.Image);
+                }
+                catch (Exception e)
+                {
+                    ModelState.AddModelError("", "Clound not delete photo");
+                    return View(raceVM);
+                }
+                var photoResult = await _photoService.AddPhotoAsync(raceVM.Image);
+                var race = new Race
+                {
+                    Id = id,
+                    Title = raceVM.Title,
+                    Description = raceVM.Description,
+                    Image = photoResult.Url.ToString(),
+                    AddressId = raceVM.AddressId,
+                    Address = raceVM.Address,
+                };
+
+                _raceRepository.Update(race);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(raceVM);
+            }
+        }
+
     }
 }
