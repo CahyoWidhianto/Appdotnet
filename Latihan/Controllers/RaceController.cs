9d3a7ae94dce177f213c258f0cb0fb6c33c3e2ff
CahyoWidhianto <widhiantoc@gmail.com>
2023-04-19 21:23:12 +0700

    Delete Club && Race

--- Latihan/Controllers/RaceController.cs
+++ Latihan/Controllers/RaceController.cs
@@ -119,6 +119,21 @@ namespace Latihan.Controllers
                 return View(raceVM);
             }
         }
+        public async Task<IActionResult> Delete(int id)
+        {
+            var raceDetails = await _raceRepository.GetByIdAsync(id);
+            if (raceDetails == null) return View("Error");
+            return View(raceDetails);
+        }
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult>DeleteRace(int id)
+        {
+            var raceDetails = await _raceRepository.GetByIdAsync(id);
+            if (raceDetails == null) return View("Error");
+
+            _raceRepository.Delete(raceDetails);
+            return RedirectToAction("Index");
+        }
 
     }
 }
