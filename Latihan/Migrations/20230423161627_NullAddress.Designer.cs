fc122a9c98ce1b069254cdba5a06e3a711089bd7
CahyoWidhianto <widhiantoc@gmail.com>
2023-04-23 23:22:16 +0700

    Register User

--- Latihan/Migrations/20230423161627_NullAddress.Designer.cs
+++ Latihan/Migrations/20230423161627_NullAddress.Designer.cs
@@ -4,6 +4,7 @@ using Latihan.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Latihan.Migrations
 {
     [DbContext(typeof(AplicationDbContext))]
-    partial class AplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230423161627_NullAddress")]
+    partial class NullAddress
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
