ced6cc7e59627acba638f29100879393f0cc6eb4
CahyoWidhianto <widhiantoc@gmail.com>
2023-05-01 00:22:38 +0700

    List && Details User

---
+++ Latihan/Controllers/UserController.cs
@@ -0,0 +1,48 @@
+﻿using Latihan.Interfaces;
+using Latihan.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Latihan.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IUserRepository _userRepository;
+        public UserController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        [HttpGet("Users")]
+        public async Task<IActionResult> Index()
+        {
+            var users = await _userRepository.GetAllUsers();
+            List<UsersViewModel> result = new List<UsersViewModel>();
+            foreach (var user in users)
+            {
+                var userViewModel = new UsersViewModel()
+                {
+                    Id = user.Id,
+                    UserName = user.UserName,
+                    Pace = user.Pace,
+                    Mileage = user.Mileage
+                };
+                result.Add(userViewModel);
+            }
+            return View(result);
+        }
+
+        public async Task<IActionResult> Detail(string id)
+        {
+            var user = await _userRepository.GetUserById(id);
+            var userDetailViewModel = new UserDetailViewModel()
+            {
+                Id = user.Id,
+                UserName = user.UserName,
+                Pace = user.Pace,
+                Mileage = user.Mileage,
+            };
+            return View(userDetailViewModel);
+
+        }
+    }
+}
---
+++ Latihan/Repository/UserRepository.cs
@@ -0,0 +1,47 @@
+﻿using Latihan.Data;
+using Latihan.Interfaces;
+using Latihan.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Latihan.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly AplicationDbContext _context;
+        public UserRepository(AplicationDbContext context)
+        {
+            _context = context;
+        }
+        public bool Add(AppUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(AppUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<AppUser>> GetAllUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        public async Task<AppUser> GetUserById(string id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+
+        public bool Save()
+        {
+            var saved = _context.SaveChanges();
+            return saved > 0 ? true : false;
+        }
+
+        public bool Update(AppUser user)
+        {
+            _context.Users.Update(user);
+            return Save();
+        }
+    }
+}
